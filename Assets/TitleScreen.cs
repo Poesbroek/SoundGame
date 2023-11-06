fbff023b9a1ae4d5329c48549c55d6f23b338af2
Silas Peters <silaspeters03@gmail.com>
2023-11-06 21:02:38 +0100

    Finished titlescreen

--- Assets/TitleScreen.cs
+++ Assets/TitleScreen.cs
@@ -1,25 +1,46 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))]
 public class TitleScreen : MonoBehaviour
 {
-    [SerializeField] AudioSource source;
+    public AudioClip ringtone;
+    public AudioClip theTalk;
 
-    void Start()
+    private bool _playing;
+    private AudioSource _audioSource;
+
+    private void Start()
     {
-        source = GetComponent<AudioSource>();
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.PlayOneShot(ringtone);
     }
 
     void Update()
     {
+        if (_playing) return;
         if (Input.GetKeyDown(KeyCode.Escape))
-            Application.Quit();
-
-        if (Input.anyKeyDown)
         {
-
+            Application.Quit();
+            return; // Also just to be sure
         }
+
+        if (Input.anyKeyDown) // Aside from Escape
+        {
+            _playing = true;
+            StartCoroutine(PickUpPhone());
+        }
+    }
+
+    private IEnumerator PickUpPhone()
+    {
+        _audioSource.Stop();
+        // TODO answer phone sound
+        yield return new WaitForSeconds(1);
+        _audioSource.PlayOneShot(theTalk);
+        yield return new WaitWhile(() => _audioSource.isPlaying);
+        SceneManager.LoadScene(1); // Assumes this is scene 0. Quick and dirty.
     }
 }
