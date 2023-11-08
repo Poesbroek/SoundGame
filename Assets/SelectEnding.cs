35f2a88ac92f28a2bb9387a181dbdd7b76aa5c8c
Poesbroek <78036828+Poesbroek@users.noreply.github.com>
2023-11-08 17:32:49 +0100

    [Fix] Win event

--- Assets/SelectEnding.cs
+++ Assets/SelectEnding.cs
@@ -1,19 +1,37 @@
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class SelectEnding : MonoBehaviour
 {
     [SerializeField] int MemeEndingChance = 100;
-    [SerializeField] UnityEvent normalEnding;
-    [SerializeField] UnityEvent memeEnding;
+    [SerializeField] private string mainSceneName;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip normalClip;
+    [SerializeField] private AudioClip memeClip;
 
+    private bool audioToggle = false
+        ;
     private void Awake()
     {
         float random = Random.Range(0, MemeEndingChance);
-        if (random == MemeEndingChance)
-            normalEnding.Invoke();
-        else 
-            memeEnding.Invoke();
-
+        if(random == MemeEndingChance) 
+        {
+            audioSource.PlayOneShot(memeClip);
+            audioToggle = true;
+        }
+        else
+        {
+            audioSource.PlayOneShot(normalClip);
+            audioToggle = true;
+        }
     }
+    private void Update()
+    {
+        if(audioToggle && !audioSource.isPlaying) 
+        {
+            SceneManager.LoadScene(mainSceneName);
+            this.enabled = false;
+        }
+    }
+
 }
